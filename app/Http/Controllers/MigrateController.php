bdbf99de3c730cc3379bb5d4fb9b4562e0ee2dc7
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2021-05-10 11:56:18 +0300

    Start project

---
+++ app/Http/Controllers/MigrateController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class MigrateController extends Controller
+{
+    public function CategoryMigrate(){
+    	return 'Category Migrate';
+	}
+}
